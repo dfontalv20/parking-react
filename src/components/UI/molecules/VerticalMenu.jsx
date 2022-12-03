cce368f7e96a712aa10652455bfe171d71b2e73e
Diego Fontalvo <d.andres2002@hotmail.com>
2022-12-03 17:24:32 -0500

    Agregado Bonzi

--- src/components/UI/molecules/VerticalMenu.jsx
+++ src/components/UI/molecules/VerticalMenu.jsx
@@ -1,27 +1,48 @@
-import { Box, Tab, Tabs } from '@mui/material';
-import React from 'react';
+import { LocalParking } from "@mui/icons-material";
+import { Box, Divider, Tab, Tabs, Typography } from "@mui/material";
+import Bonzi from "../../../assets/BonziBuddy.webp";
+import React from "react";
 
-const VerticalMenu = props => {
+const VerticalMenu = (props) => {
+  const { value, onChange, options } = props;
 
-    const { value, onChange, options } = props
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display={'flex'}
+      flexGrow={1}
+      flexDirection="column"
+      textAlign="center"
+    >
+      <Box marginY={5}>
+        <LocalParking style={{ fontSize: "64px" }} color="primary" />
+        <Typography variant="h2" fontSize={20} fontWeight={700}>
+          Gestion de parqueo
+        </Typography>
+      </Box>
+      <Tabs
+        style={{ width: "100%" }}
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={onChange}
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        {options.map((option, key) => (
+          <Tab
+            key={key}
+            label={option.label}
+            icon={option.icon ?? <></>}
+            iconPosition="start"
+          />
+        ))}
+      </Tabs>
+      <Box marginTop={'auto'}>
+        <img src={Bonzi} alt="Bonzi Buddy" width={'40%'}/>
+      </Box>
+    </Box>
+  );
+};
 
-    return (
-        <Box
-            sx={{ flexGrow: 1, display: 'flex', height: '100%', width: '100%' }}
-        >
-            <Tabs
-                style={{width: '100%'}}
-                orientation="vertical"
-                variant="scrollable"
-                value={value}
-                onChange={onChange}
-                sx={{ borderRight: 1, borderColor: 'divider' }}
-            >
-                {options.map((option, key) => <Tab key={key} label={option.label} icon={option.icon ?? <></>} iconPosition="start"/>)}
-            </Tabs>
-        </Box>
-
-    );
-}
-
-export default VerticalMenu
+export default VerticalMenu;
